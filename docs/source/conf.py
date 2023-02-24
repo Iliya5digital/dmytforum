a804387e520ee891abb417b9f834c9e66149cbb8
Iliya5digital <115736491+Iliya5digital@users.noreply.github.com>
2023-02-24 09:10:51 +0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'dmytforum'
+copyright = '2023, Iliya'
+author = 'Iliya'
 
 release = '0.1'
 version = '0.1.0'
